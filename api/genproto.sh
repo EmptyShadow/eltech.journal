0237049f759245f72e02495822daf359c25c4c51
Nebotov Dmitry <dimon6407@gmail.com>
2020-11-02 13:01:55 +0300

    Поправил генерацию гошного кода grpc

--- api/genproto.sh
+++ api/genproto.sh
@@ -3,8 +3,13 @@ for f in *.proto; do
   fn="${f%.*}"
   mkdir go js doc
   docker run --rm -v $(pwd):$(pwd) -w $(pwd) emptyshadow/protoc:v1.33.0 \
-    -I=. -I=/usr/include -I=/usr/include/github.com/gogo/protobuf/protobuf \
-    --gogoslick_out=plugins=grpc:go \
+    -I=. \
+    --gogoslick_out=plugins=grpc,\
+Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:go \
     --js_out=import_style=commonjs:js \
     --grpc-web_out=import_style=commonjs,mode=grpcwebtext:js \
     --doc_out=doc --doc_opt=markdown,api."${fn}".md \
