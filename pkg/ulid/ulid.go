24ca16cb100571b78af4305f0dcd4378572c40e4
Nebotov Dmitry <dimon6407@gmail.com>
2021-01-07 14:53:47 +0300

    Добавлен пакет для ulid

---
+++ pkg/ulid/ulid.go
@@ -0,0 +1,14 @@
+package ulid
+
+import (
+	"math/rand"
+
+	"github.com/oklog/ulid/v2"
+)
+
+func MustNew() string {
+	t := ulid.Time(ulid.Now())
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+
+	return ulid.MustNew(ulid.Timestamp(t), entropy).String()
+}
