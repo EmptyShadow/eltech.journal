5fdebb9b6ce42652e3ca94513d726fd0e6e0166d
brdnkva <bev030997@gmail.com>
2021-01-18 19:51:23 +0300

    Добавлен макет таблицы ведомости

--- web/frontend/src/pages/home/components/courseTable.jsx
+++ web/frontend/src/pages/home/components/courseTable.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import { Table, Tag, Space } from 'antd';
+import { Table} from 'antd';
+
+const columns = [
+    {
+        title: 'Задание',
+        dataIndex: 'task',
+        key: 'task',
+    },
+    {
+        title: 'Тип',
+        dataIndex: 'type',
+        key: 'type'
+    },
+    {
+        title: 'Срок сдачи',
+        dataIndex: 'deadline',
+        key: 'deadline',
+    },
+    {
+        title: 'Группы',
+        dataIndex: 'groups',
+        key: 'groups',
+    }
+];
+
+const dataSource = [
+    {
+        key: '1',
+        task: 'Исследование систем',
+        type: 'Курсовая работа',
+        deadline: '10 октября',
+        groups: 'ff'
+    }
+];
 
 const CourseTable = () => {
     return (
-        <Table>
-        </Table>
+        <Table columns={columns} dataSource={dataSource} />
     );
 }
 
